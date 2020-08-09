df3afff487b3184fc8c3227a5c59d11803f85312
Ryota-1 <stream.of.tears@icloud.com>
2020-08-09 22:01:40 +0900

    Second commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '880dfee30042d2cd7a266eae5277430822b54d872f7281bca9899f69ffed55a7edecba7bc43fab78b0c4de8de448ae1eda135de0b120cb115674c687edce2b31'
