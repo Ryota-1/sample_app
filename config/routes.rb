7c1d75dfc7dd164bdb1e81d1bdfc6a6ca2ae4234
Ryota-1 <stream.of.tears@icloud.com>
2020-08-10 15:12:32 +0900

    Finish sample_app

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
   # .../todolists/1 や .../todolists/3 に該当する
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
